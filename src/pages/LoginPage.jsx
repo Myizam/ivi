3b7637d06ad2c3f47e415c317f9d1cf02d56e172
MacBook <macbook@MacBook-Pro-MacBook.local>
2022-11-01 15:13:02 +0600

    virs

---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const LoginPage = () => {
+  return (
+    <div className='ddd1'>
+        <h2 className='hhh2'>Авторизация</h2>
+        <input className='iii3' type="text" placeholder='Username' />
+        <input className='iii3' type="text" placeholder='Password' />
+
+        <button className='bbb4' >Авторизация</button>
+    </div>
+  )
+}
+
+export default LoginPage
---
+++ src/pages/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const NotFoundPage = () => {
+  return (
+    <div style={{color: "white"}}>NotFoundPage</div>
+  )
+}
+
+export default NotFoundPage
